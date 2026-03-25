462d68c7390f3c336d8da58bf171271e00fbd572
Caitlin Teague Doerr <design@caitlinteague.com>
2026-03-24 22:01:07 -0500

    update margin top for referrer value

--- src/components/Traffic.js
+++ src/components/Traffic.js
@@ -1,107 +1,109 @@
-import React from 'react';
+import React from "react";
 
 // constants
-import * as Constants from './data/constants';
+import * as Constants from "./data/constants";
 
 // visual components
-import {Section} from './visual/Section';
-import {TimeStamp} from './visual/TimeStamp';
-import {ValueChecker} from './visual/ValueChecker';
+import { Section } from "./visual/Section";
+import { TimeStamp } from "./visual/TimeStamp";
+import { ValueChecker } from "./visual/ValueChecker";
 
-export class Traffic extends React.Component
-{
-	// some things to prevent memory leak
-	visible = false;
-	initialRun = true;
+export class Traffic extends React.Component {
+  // some things to prevent memory leak
+  visible = false;
+  initialRun = true;
 
-	constructor(props) 
-	{
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			datetime: new Date(),
-			topReferrer: "http://google.com",
-			referral_count: 28,
-			pageloads: 561
-		}
-	}
+    this.state = {
+      datetime: new Date(),
+      topReferrer: "http://google.com",
+      referral_count: 28,
+      pageloads: 561,
+    };
+  }
 
-	componentDidMount()
-	{
-		// this.visible = true;
+  componentDidMount() {
+    // this.visible = true;
+    // if(this.visible) {
+    // 	// fetch data the first time
+    // 	this.timer = setInterval(
+    // 		() => this.fetchData(),
+    // 		1000
+    // 	);
+    // }
+  }
 
-		// if(this.visible) {
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
 
-		// 	// fetch data the first time
-		// 	this.timer = setInterval(
-		// 		() => this.fetchData(),
-		// 		1000
-		// 	);	
-		// }
-	}
+  fetchData() {
+    // after initial, reset timer
+    if (this.initialRun) {
+      clearInterval(this.timer);
 
-	componentWillUnmount() {
-		clearInterval(this.timer);
-	}
+      this.initialRun = false;
+      this.timer = setInterval(() => this.fetchData(), 300000);
+    }
 
-	fetchData()
-	{
-		// after initial, reset timer
-		if(this.initialRun) {
-			clearInterval(this.timer);
+    //fetch the data
+    fetch(Constants.ENDPOINT + "traffic")
+      .then((results) => {
+        return results.json();
+      })
+      .then((data) => {
+        this.setState({
+          datetime: new Date(),
+          topReferrer: data.TopReferrer,
+          referral_count: data.NumReferrals,
+          pageloads: data.PageLoads,
+        });
+      })
+      .catch(function (err) {
+        console.log("Fetch Error :-S", err);
+      });
+  }
 
-			this.initialRun = false;
-			this.timer = setInterval(
-				() => this.fetchData(),
-				300000
-			);
-		}
+  render() {
+    let topReferrer;
 
-		//fetch the data
-		fetch(Constants.ENDPOINT+'traffic')
-		.then(results => {
-			return results.json();
-		})
-		.then(data => {
+    if (this.state.topReferrer) {
+      topReferrer = (
+        <a
+          href={this.state.topReferrer}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-link truncate"
+        >
+          {this.state.topReferrer}
+        </a>
+      );
+    } else {
+      topReferrer = <p>Unknown</p>;
+    }
 
-			this.setState({
-				datetime: new Date(),
-				topReferrer: data.TopReferrer,
-				referral_count: data.NumReferrals,
-				pageloads: data.PageLoads
-			});
-		})
-		.catch(function(err) {
-			console.log('Fetch Error :-S', err);
-		});
-	}
+    return (
+      <React.Fragment>
+        <div className="content">
+          <Section name="Page loads">
+            <ValueChecker
+              value={this.state.pageloads}
+              threshold={50000}
+              comparison="greaterthan"
+            />
+          </Section>
+          <Section name="Top referrer">
+            {topReferrer}
+            <div className="value" style={{ marginTop: "0.5rem" }}>
+              {this.state.referral_count.toLocaleString()}
+            </div>
+          </Section>
+        </div>
 
-	render()
-	{
-		let topReferrer;
-
-		if(this.state.topReferrer)
-		{
-			topReferrer = <a href={this.state.topReferrer} target="_blank" rel="noopener noreferrer" className="text-link truncate">{this.state.topReferrer}</a>
-		}
-		else {
-			topReferrer = <p>Unknown</p>
-		}
-
-		return (
-			<React.Fragment>
-				<div className="content">
-					<Section name="Page loads">
-						<ValueChecker value={this.state.pageloads} threshold={50000} comparison="greaterthan" />
-					</Section>
-					<Section name="Top referrer">
-						{ topReferrer }
-						<div className="value">{this.state.referral_count.toLocaleString()}</div> 
-					</Section>
-				</div>
-					
-				<TimeStamp datetime={this.state.datetime} />
-			</React.Fragment>			
-		);
-	}
+        <TimeStamp datetime={this.state.datetime} />
+      </React.Fragment>
+    );
+  }
 }
