5885d86b02cf34bec098987a01d297b98f697632
Caitlin Teague Doerr <design@caitlinteague.com>
2026-06-07 12:32:14 -0500

    update modal animation

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,54 +1,68 @@
-import React from 'react';
+import React from "react";
 
 // visual components
-import { Section } from './visual/Section';
+import { Section } from "./visual/Section";
 
-export function Settings(props)
-{
-	// for getting the theme list
-	let themesList = [];
+export function Settings(props) {
+  // for getting the theme list
+  let themesList = [];
 
-	for (var theme in props.themes) 
-	{
-		const thisTheme = props.themes[theme];
-		
-		var isCurrentTheme = false;
- 		if(thisTheme.id === props.activeTheme) isCurrentTheme = true;
+  for (var theme in props.themes) {
+    const thisTheme = props.themes[theme];
 
-		themesList.push(
-			<div key={thisTheme.id} className="an-option">
-	         <input type="radio" name="theme-selection" id={"radio_"+thisTheme.id} checked={isCurrentTheme} onChange={(e) => props.switchThemes(thisTheme.id)} />
-	         <label htmlFor={"radio_"+thisTheme.id}>{thisTheme.name}</label>
-	      </div>
-		);
-	}
+    var isCurrentTheme = false;
+    if (thisTheme.id === props.activeTheme) isCurrentTheme = true;
 
-	// for getting modules list
-	let modulesList = [];
+    themesList.push(
+      <div key={thisTheme.id} className="an-option">
+        <input
+          type="radio"
+          name="theme-selection"
+          id={"radio_" + thisTheme.id}
+          checked={isCurrentTheme}
+          onChange={(e) => props.switchThemes(thisTheme.id)}
+        />
+        <label htmlFor={"radio_" + thisTheme.id}>{thisTheme.name}</label>
+      </div>,
+    );
+  }
 
-	for (var module in props.modules) 
-	{
-		const thisModule = props.modules[module];
-		
-		modulesList.push(
-			<div key={thisModule.id} className="an-option">
-	         <input type="checkbox" id={"check_"+thisModule.id} checked={thisModule.visible} onChange={(e) => props.toggleModules(thisModule)}/>
-			   <label htmlFor={"check_"+thisModule.id}>{thisModule.name}</label>
-	      </div>
-		);
-	}
+  // for getting modules list
+  let modulesList = [];
 
-	// <button type="button" className="button" onClick={props.toggleModal}>Cancel</button>
-	return (
-		<React.Fragment>
-			<div className="modal-content">
-				<Section name="Modules"> { modulesList } </Section>
-			   <Section name="Themes"> { themesList } </Section>
-		   </div>
+  for (var module in props.modules) {
+    const thisModule = props.modules[module];
 
-		   <div className="settings-actions">
-		   	<button type="button" className="button" onClick={(e) => props.saveSettings(e)}>Save</button>
-		   </div>
-		</React.Fragment>	
-	)
+    modulesList.push(
+      <div key={thisModule.id} className="an-option">
+        <input
+          type="checkbox"
+          id={"check_" + thisModule.id}
+          checked={thisModule.visible}
+          onChange={(e) => props.toggleModules(thisModule)}
+        />
+        <label htmlFor={"check_" + thisModule.id}>{thisModule.name}</label>
+      </div>,
+    );
+  }
+
+  // <button type="button" className="button" onClick={props.toggleModal}>Cancel</button>
+  return (
+    <React.Fragment>
+      <div className="modal-content">
+        <Section name="Modules"> {modulesList} </Section>
+        <Section name="Themes"> {themesList} </Section>
+      </div>
+
+      <div className="settings-actions">
+        <button
+          type="button"
+          className="button"
+          onClick={(e) => props.saveSettings(e)}
+        >
+          Save
+        </button>
+      </div>
+    </React.Fragment>
+  );
 }
